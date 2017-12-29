73bdb35af1edaf4dfd19aa327c1dfb5c9cc41066
Max <maxnelson997@gmail.com>
2017-12-29 14:27:43 -0700

    first commit

---
+++ assets/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Http , HttpModule } from '@angular/http';
+import { AppComponent } from "./app.component";
+import { MessageComponent } from './messages/message.component';
+import { MessageListComponent } from './messages/message-list.component';
+import { MessageInputComponent } from './messages/message-input.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AuthenticationComponent } from './auth/authentication.component';
+import { HeaderComponent } from './header.component'
+import { routing } from './app.routing';
+import { LogoutComponent } from './auth/logout.component';
+import { SignupComponent } from './auth/signup.component';
+import { SigninComponent } from './auth/signin.component';
+
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        MessageComponent,
+        MessageListComponent,
+        MessageInputComponent,
+        MessagesComponent,
+        AuthenticationComponent,
+        HeaderComponent,
+        LogoutComponent,
+        SignupComponent,
+        SigninComponent
+    ],
+    imports: [BrowserModule, FormsModule, routing, HttpModule, ReactiveFormsModule],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+
+}
---
+++ assets/app/messages/message.service.ts
@@ -0,0 +1,31 @@
+import { Message } from './message.model';
+import { Http, Response, Headers } from '@angular/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import 'rxjs/Rx';
+
+@Injectable()
+export class MessageService {
+    messages: Message[] = []
+
+    constructor(private http: Http) {}
+
+    addMessage(message: Message) {
+        this.messages.push(message)
+        const body = JSON.stringify(message);
+        const headers = new Headers({'Content-Type': 'application/json', 'Accept': 'application/json'})
+    
+        
+        return this.http.post('http://localhost:3000/message', body, {headers: headers})
+            .map((response: Response) => response.json())
+            .catch((error: Response) => error.json());
+    }
+    
+    getMessages() {
+        return this.messages
+    }
+
+    deleteMessage(message: Message) {
+        this.messages.splice(this.messages.indexOf(message), 1)
+    }
+}
